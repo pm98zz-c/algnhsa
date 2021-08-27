0b0242832fdda76ef3d11f098622ff3cc3072c2e
Pascal Morin <pm98zz.c@gmail.com>
2021-08-27 15:58:37 +0200

    Update request.go

--- request.go
+++ request.go
@@ -1,10 +1,10 @@
 package algnhsa
 
 import (
+	"bytes"
 	"context"
 	"encoding/base64"
 	"errors"
-	"io"
 	"net/http"
 	"net/url"
 	"strings"
@@ -53,64 +53,41 @@ func newLambdaRequest(ctx context.Context, payload []byte, opts *Options) (lambd
 }
 
 func newHTTPRequest(event lambdaRequest) (*http.Request, error) {
-	// Build request URL.
-	params := url.Values{}
-	for k, v := range event.QueryStringParameters {
-		params.Set(k, v)
-	}
-	for k, vals := range event.MultiValueQueryStringParameters {
-		params[k] = vals
-	}
-
-	// Set headers.
-	// https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-	// If you specify values for both headers and multiValueHeaders, API Gateway merges them into a single list.
-	// If the same key-value pair is specified in both, only the values from multiValueHeaders will appear
-	// the merged list.
-	headers := make(http.Header)
-	for k, v := range event.Headers {
-		headers.Set(k, v)
-	}
-	for k, vals := range event.MultiValueHeaders {
-		headers[http.CanonicalHeaderKey(k)] = vals
-	}
-
-	u := url.URL{
-		Host:     headers.Get("host"),
-		RawPath:  event.Path,
-		RawQuery: params.Encode(),
-	}
-
-	// Unescape request path
-	p, err := url.PathUnescape(u.RawPath)
-	if err != nil {
-		return nil, err
-	}
-	u.Path = p
-
-	if u.Path == u.RawPath {
-		u.RawPath = ""
-	}
-
-	// Handle base64 encoded body.
-	var body io.Reader = strings.NewReader(event.Body)
+	decodedBody := []byte(event.Body)
 	if event.IsBase64Encoded {
-		body = base64.NewDecoder(base64.StdEncoding, body)
+		base64Body, err := base64.StdEncoding.DecodeString(event.Body)
+		if err != nil {
+			return nil, err
+		}
+		decodedBody = base64Body
 	}
-
-	// Create a new request.
-	r, err := http.NewRequest(event.HTTPMethod, u.String(), body)
+	path := event.Path
+	if len(event.MultiValueQueryStringParameters) > 0 {
+		queryString := ""
+		for q, l := range event.MultiValueQueryStringParameters {
+			for _, v := range l {
+				if queryString != "" {
+					queryString += "&"
+				}
+				queryString += q + "=" + v
+			}
+		}
+		path += "?" + queryString
+	}
+	httpRequest, err := http.NewRequest(
+		strings.ToUpper(event.HTTPMethod),
+		path,
+		bytes.NewReader(decodedBody),
+	)
 	if err != nil {
 		return nil, err
 	}
 
-	// Set remote IP address.
-	r.RemoteAddr = event.SourceIP
-
-	// Set request URI
-	r.RequestURI = u.RequestURI()
-
-	r.Header = headers
-
-	return r.WithContext(event.Context), nil
+	for k, values := range event.MultiValueHeaders {
+		for _, value := range values {
+			httpRequest.Header.Add(k, value)
+		}
+	}
+	httpRequest.RequestURI = httpRequest.URL.RequestURI()
+	return httpRequest.WithContext(event.Context), nil
 }
